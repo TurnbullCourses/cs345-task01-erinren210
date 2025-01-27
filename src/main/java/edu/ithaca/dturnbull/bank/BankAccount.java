c3bad44fe753ca0213ba314a4b30efe8fc5c74e5
Brooke Wilson <brookewil@comcast.net>
2025-01-27 08:07:24 -0500

    implemented isEmailValid, only first test (a@b.com) fails

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -40,11 +40,44 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+        String lowEmail = email.toLowerCase();
+
+        // email can not be empty or not contain @
+        if (email == null || email.isEmpty() || email.indexOf('@') == -1) {
             return false;
         }
-        else {
-            return true;
-        }
+
+        for (int i = 0; i <= lowEmail.length(); i++){
+            char c = lowEmail.charAt(i);
+            // check email contains a-z and/or 0-9
+            if ((c >= 'a' && c <= 'z') || (c >= 0 && c <= 9)){
+                continue;
+            } 
+
+            if ((Character.valueOf(c).equals(".")) || (Character.valueOf(c).equals("-")) || (Character.valueOf(c).equals("_"))){
+
+                // cannot be a special character at the beginning of end of email
+                if (i == 0 || i == (lowEmail.length() - 1)){
+                    return false;
+                }
+
+                // cannot have two special characters in a row
+                if (i > 0 && (lowEmail.charAt(i - 1) == '.' || lowEmail.charAt(i - 1) == '-' || lowEmail.charAt(i - 1) == '_')){
+                    return false;
+                }
+
+            } else if (c == '@') {
+                // can only contain one "@", cannot be at beginning or end
+                if (i == 0 || i == lowEmail.length() - 1 || lowEmail.indexOf('@') != lowEmail.lastIndexOf('@')) {
+                    return false;
+                }
+
+            } else {
+                // everything else is false
+                return false;
+            }
+        } 
+        // everything else is valid
+        return true;
     }
 }
