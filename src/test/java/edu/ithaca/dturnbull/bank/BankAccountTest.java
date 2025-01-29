60372edcacb8fafa9529e5561060518e851516f2
Erin <ecummings@ithaca.edu>
2025-01-28 22:03:13 -0500

    updated constructorTest

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -64,6 +64,7 @@ class BankAccountTest {
         assertEquals(200, bankAccount.getBalance(), 0.001);
         //check for exception thrown correctly
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("a@b.com", 100.01));
     }
 
     @Test
